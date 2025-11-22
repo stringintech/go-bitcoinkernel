1f06f2a056baf7c6f55e5435891e487e4a3610bc
stringintech <stringintech@gmail.com>
2025-11-22 11:01:49 +0330

    Add TransactionInput iterators for Transaction
    Also adds test for iterators and groups Transaction tests into subtests.

--- kernel/transaction.go
+++ kernel/transaction.go
@@ -5,6 +5,7 @@ package kernel
 */
 import "C"
 import (
+	"iter"
 	"unsafe"
 )
 
@@ -66,11 +67,6 @@ func (t *transactionApi) Copy() *Transaction {
 	return newTransaction(t.ptr, false)
 }
 
-// CountInputs returns the number of inputs in the transaction.
-func (t *transactionApi) CountInputs() uint64 {
-	return uint64(C.btck_transaction_count_inputs(t.ptr))
-}
-
 // CountOutputs returns the number of outputs in the transaction.
 func (t *transactionApi) CountOutputs() uint64 {
 	return uint64(C.btck_transaction_count_outputs(t.ptr))
@@ -92,22 +88,6 @@ func (t *transactionApi) GetOutput(index uint64) (*TransactionOutputView, error)
 	return newTransactionOutputView(check(ptr)), nil
 }
 
-// GetInput retrieves the input at the specified index.
-//
-// The returned input is a non-owned view that depends on the lifetime of this transaction.
-//
-// Parameters:
-//   - index: Index of the input to retrieve
-//
-// Returns an error if the index is out of bounds.
-func (t *transactionApi) GetInput(index uint64) (*TransactionInputView, error) {
-	if index >= t.CountInputs() {
-		return nil, ErrKernelIndexOutOfBounds
-	}
-	ptr := C.btck_transaction_get_input_at(t.ptr, C.size_t(index))
-	return newTransactionInputView(check(ptr)), nil
-}
-
 // Bytes returns the consensus serialized representation of the transaction.
 //
 // Returns an error if the serialization fails.
@@ -125,4 +105,96 @@ func (t *transactionApi) Bytes() ([]byte, error) {
 func (t *transactionApi) GetTxid() *TxidView {
 	ptr := C.btck_transaction_get_txid(t.ptr)
 	return newTxidView(check(ptr))
+}
+
+// CountInputs returns the number of inputs in the transaction.
+func (t *transactionApi) CountInputs() uint64 {
+	return uint64(C.btck_transaction_count_inputs(t.ptr))
+}
+
+// GetInput retrieves the input at the specified index.
+//
+// The returned input is a non-owned view that depends on the lifetime of this transaction.
+//
+// Parameters:
+//   - index: Index of the input to retrieve
+//
+// Returns an error if the index is out of bounds.
+func (t *transactionApi) GetInput(index uint64) (*TransactionInputView, error) {
+	if index >= t.CountInputs() {
+		return nil, ErrKernelIndexOutOfBounds
+	}
+	ptr := C.btck_transaction_get_input_at(t.ptr, C.size_t(index))
+	return newTransactionInputView(check(ptr)), nil
+}
+
+// Inputs returns an iterator over all inputs in the transaction.
+//
+// The returned inputs are non-owned views that depend on the lifetime of this transaction.
+//
+// Example usage:
+//
+//	for input := range tx.Inputs() {
+//	    // Process input
+//	}
+func (t *transactionApi) Inputs() iter.Seq[*TransactionInputView] {
+	return func(yield func(*TransactionInputView) bool) {
+		t.iterInputs(0, t.CountInputs(), yield)
+	}
+}
+
+// InputsRange returns an iterator over a range of inputs in the transaction.
+//
+// Parameters:
+//   - from: Starting index (inclusive)
+//   - to: Ending index (exclusive)
+//
+// The returned inputs are non-owned views that depend on the lifetime of this transaction.
+// Safe for out-of-bounds arguments: 'to' is clamped to the count,
+// and an invalid range (from >= to) yields an empty iterator.
+//
+// Example usage:
+//
+//	for input := range tx.InputsRange(0, 5) {
+//	    // Process inputs 0-4
+//	}
+func (t *transactionApi) InputsRange(from, to uint64) iter.Seq[*TransactionInputView] {
+	return func(yield func(*TransactionInputView) bool) {
+		if count := t.CountInputs(); to > count {
+			to = count
+		}
+		t.iterInputs(from, to, yield)
+	}
+}
+
+// InputsFrom returns an iterator over inputs starting from the given index.
+//
+// Parameters:
+//   - from: Starting index (inclusive)
+//
+// The returned inputs are non-owned views that depend on the lifetime of this transaction.
+// If from is beyond the input count, returns an empty iterator.
+//
+// Example usage:
+//
+//	for input := range tx.InputsFrom(5) {
+//	    // Process inputs from index 5 to the end
+//	}
+func (t *transactionApi) InputsFrom(from uint64) iter.Seq[*TransactionInputView] {
+	return func(yield func(*TransactionInputView) bool) {
+		t.iterInputs(from, t.CountInputs(), yield)
+	}
+}
+
+// iterInputs is a helper that iterates over inputs in [from, to).
+func (t *transactionApi) iterInputs(from, to uint64, yield func(*TransactionInputView) bool) {
+	for i := from; i < to; i++ {
+		input, err := t.GetInput(i)
+		if err != nil {
+			panic(err)
+		}
+		if !yield(input) {
+			return
+		}
+	}
 }
